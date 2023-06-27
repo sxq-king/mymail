1e407d1c64821066b0c4f78c2c09c4f1d5aec22e
sxq <996957240@qq.com>
2023-06-27 15:56:42 +0800

    修改信息

--- src/main/java/com/sxq/mymail/common/Constants.java
+++ src/main/java/com/sxq/mymail/common/Constants.java
@@ -6,9 +6,12 @@ public interface Constants {
     String MAIL_USERNAME = "996957240@qq.com";
     String MAIL_PASSWORD = "zsgiaqktbdhibdcf";
 
-    public static String L_Y_H = "1879705300@qq.com";
-    public static String D_J = "www.2066573721@qq.com";
-    public static String L_T= "j2568095536@Gmail.com";
-    public static String M_F_S = "M_F_S";
-    public static String Y_A_D = "Y_A_D";
+    /**
+     * 沙雕邮箱
+     */
+    public String L_Y_H = "1879705300@qq.com";
+    public String D_J = "www.2066573721@qq.com";
+    public String L_T= "j2568095536@Gmail.com";
+    public String M_F_S = "3109973858@qq.com";
+    public String Y_A_D = "3232280141@qq.com";
 }
